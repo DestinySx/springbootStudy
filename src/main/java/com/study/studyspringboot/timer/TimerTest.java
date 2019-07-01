4e302b2cb644f36c1b0a3afcbc284c0827214986
suxin <981828064@qq.com>
2019-07-01 17:20:09 +0800

    springboot学习

---
+++ src/main/java/com/study/studyspringboot/timer/TimerTest.java
@@ -0,0 +1,22 @@
+package com.study.studyspringboot.timer;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: suxin
+ * Date: 2019/7/1   Time: 17:03
+ * Description:
+ **/
+@Component
+public class TimerTest {
+
+    @Scheduled(cron = "0/1 * * * * ?")
+    // http://cron.qqe2.com/   配置定时时间
+    private void test() {
+        System.out.println("执行定时任务的时间是："+new Date());
+    }
+}
