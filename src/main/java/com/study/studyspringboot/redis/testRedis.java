76d7e4fa9ed924732c8ee448fa0f1c18bedcd74b
suxin <981828064@qq.com>
2019-07-11 19:59:08 +0800

    计算月份差距

--- src/main/java/com/study/studyspringboot/redis/testRedis.java
+++ src/main/java/com/study/studyspringboot/redis/testRedis.java
@@ -1,6 +1,7 @@
 package com.study.studyspringboot.redis;
 
 import com.study.studyspringboot.redis.model.User;
+import org.apache.commons.lang.time.DurationFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -44,7 +46,7 @@ public class testRedis {
 
     @RequestMapping(value = "/get",produces = MediaType.APPLICATION_JSON_VALUE)
     public Object getPOJO(){
-
+        System.out.println(DurationFormatUtils.formatPeriod(new Date("2019/11").getTime(), new Date("2020/02").getTime(), "M"));
         return redisTemplate.opsForValue().get("user1");
         //return stringRedisTemplate.opsForValue().get("user1");
     }
