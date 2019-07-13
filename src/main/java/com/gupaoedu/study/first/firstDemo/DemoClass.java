66f2b4745d756bc3467a1cdb8441675f9eedbbbb
suxin <981828064@qq.com>
2019-07-14 03:41:42 +0800

    Spring注解 @CompomemtScan 注解

--- src/main/java/com/gupaoedu/study/first/firstDemo/DemoClass.java
+++ src/main/java/com/gupaoedu/study/first/firstDemo/DemoClass.java
@@ -1,11 +1,14 @@
 package com.gupaoedu.study.first.firstDemo;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created with IntelliJ IDEA.
  * User: suxin
  * Date: 2019/7/14   Time: 3:05
  * Description:
  **/
+@Service
 public class DemoClass {
 
     public String say(){
