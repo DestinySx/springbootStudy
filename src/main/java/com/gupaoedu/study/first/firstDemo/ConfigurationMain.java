c9c4aa8e98dc0b5bc6a087a6dac88e01acdf98f2
suxin <981828064@qq.com>
2019-07-14 03:35:52 +0800

    Spring注解 @CompomemtScan 注解

--- src/main/java/com/gupaoedu/study/first/firstDemo/ConfigurationMain.java
+++ src/main/java/com/gupaoedu/study/first/firstDemo/ConfigurationMain.java
@@ -2,6 +2,7 @@ package com.gupaoedu.study.first.firstDemo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,10 +11,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Description:
  **/
 @SpringBootApplication
+@ComponentScan(basePackages = "com.gupaoedu.study.first")
 public class ConfigurationMain {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationDemo.class);
-        DemoClass demoClass = applicationContext.getBean(DemoClass.class);
-        System.out.print(demoClass.say());
+//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationDemo.class);
+//        DemoClass demoClass = applicationContext.getBean(DemoClass.class);
+//        System.out.print(demoClass.say());
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationMain.class);
+
+        String[] beansName = applicationContext.getBeanDefinitionNames();
+        for(String beanName:beansName){
+            System.out.println(beanName);
+        }
     }
 }
